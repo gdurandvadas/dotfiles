b032f85936e5c8a6846bdbc404d78e90e550fd26
Gastón Durand Vadas <gdurandvadas@gmail.com>
2022-04-02 13:48:19 +0200

    fix platform warning

--- home/config/fish/functions/swagger.fish
+++ home/config/fish/functions/swagger.fish
@@ -1,3 +1,3 @@
 function swagger
-  docker run --rm -it  --user (id -u):(id -g) -e GOPATH=(go env GOPATH):/go -v $HOME:$HOME -w (pwd) quay.io/goswagger/swagger $argv
+  docker run --platform linux/amd64 --rm -it  --user (id -u):(id -g) -e GOPATH=$GOPATH -v $HOME:$HOME -w (pwd) quay.io/goswagger/swagger $argv
 end
