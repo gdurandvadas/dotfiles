5c0b0810a32a5f76bb908470e31c66309c9c5239
Gastón Durand Vadas <gaston.vadas@mollie.com>
2022-08-17 12:53:03 +0200

    Conditional ssh config

--- scripts/templates.bash
+++ scripts/templates.bash
@@ -15,7 +15,10 @@ ${HOMEBREW_PREFIX}/bin/envsubst '$HOMEBREW_PREFIX' < templates/tmux.yaml > home/
 if test -f "is_job"; then
   export EMAIL="gaston.vadas@mollie.com"
   ${HOMEBREW_PREFIX}/bin/envsubst '$EMAIL' < templates/gitconfig > home/gitconfig
+  ${HOMEBREW_PREFIX}/bin/envsubst '$ONE_PASSWORD_SSH_CONFIG' < templates/ssh-config > home/ssh/config
 else
   export EMAIL="contact@gedv.me"
   ${HOMEBREW_PREFIX}/bin/envsubst '$EMAIL' < templates/gitconfig > home/gitconfig
+  export ONE_PASSWORD_SSH_CONFIG="Host *\n  UseKeychain yes\n  IdentityAgent ~/.1password/agent.sock"
+  ${HOMEBREW_PREFIX}/bin/envsubst '$ONE_PASSWORD_SSH_CONFIG' < templates/ssh-config > home/ssh/config
 fi
