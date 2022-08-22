834a2573933080543c9089f46fab874cece0d8a1
Gastón Durand Vadas <gaston.vadas@mollie.com>
2022-08-22 13:09:21 +0200

    define default editor

--- home/config/fish/config.fish
+++ home/config/fish/config.fish
@@ -1,6 +1,8 @@
 set -gx XDG_CONFIG_HOME "$HOME/.config"
 set -gx XDG_DATA_HOME "$HOME/.local/share"
 set -gx XDG_CACHE_HOME "$HOME/.cache"
+set -gx EDITOR "/usr/bin/vim"
+set -gx TERM "xterm"
 
 if status is-interactive
   # starship
